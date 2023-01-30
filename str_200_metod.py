33483f2d7217143ebd20be2404316de43ae94c0a
Bkey-201 <intersys114@gmail.com>
2023-01-30 22:02:29 +0300

    Add file

---
+++ str_200_metod.py
@@ -0,0 +1,14 @@
+colors = ["red", "yellow", "green", "Ыuе"]
+# Вставляет "orange" во вторую позицию
+colors.insert(1, "orange")
+print(colors)
+
+color = colors.pop(3)
+print(color)
+print(colors)
+# colors.pop(-1)
+# print(colors)
+colors.pop()
+print(colors)
+colors.append("indigo")
+print(colors)
